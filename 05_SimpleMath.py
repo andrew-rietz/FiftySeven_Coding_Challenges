e54ab5abeef37191f8dd3a292769a08188e13715
Andrew Rietz <andrew.rietz@gmail.com>
2019-04-13 00:28:22 -0600

    Challenges 03 through 10

---
+++ 05_SimpleMath.py
@@ -0,0 +1,51 @@
+'''
+
+You'll often write program that deal with numbers. And
+depending on the programming language you use, you'll
+have to convert the inputs you get to numerical data
+types.
+
+Write a program that prompts for two numbers. Print
+the sum, difference, product, and quotient of those
+numbers as shown in the example output
+
+___________________
+Example Output
+___________________
+What is the first number? 10
+What is the second number? 5
+10 + 5 = 15
+10 - 5 = 5
+10 * 5 = 50
+10 / 5 = 2
+
+___________________
+Constraints
+___________________
+Values coming from the user will be strings. Ensure
+that you convert these values to numbers before
+doing the match.
+Keep inputs and outputs separate from the numerical
+conversions and other processing.
+Generate a single output statement with line breaks
+in the appropriate spots.
+
+'''
+
+firstnum = input('What is the first number? ')
+secondnum = input('What is the second number? ')
+
+firstnum = float(firstnum)
+secondnum = float(secondnum)
+
+sum = round(firstnum + secondnum,1)
+difference = round(firstnum - secondnum,1)
+product = round(firstnum * secondnum,1)
+quotient = round(firstnum / secondnum,1)
+
+print(f'''
+    {firstnum} + {secondnum} = {sum}\n
+    {firstnum} - {secondnum} = {difference}\n
+    {firstnum} * {secondnum} = {product}\n
+    {firstnum} / {secondnum} = {quotient}
+''')
---
+++ 08_PizzaParty.py
@@ -0,0 +1,37 @@
+'''
+
+Division isn't always exact, and sometimes you'll
+write programs that will need to deal with the
+leftovers as a whole number instead of a decimal.
+
+Write a program to evenly divide pizzas. Prompt for
+the number of people, the number of pizzas, and the
+number of slices per pizza. Ensure that the number
+of pieces comes out even. Display the number of pieces
+each person should get. If there are leftovers,
+show the number of leftover pieces.
+
+___________________
+Example Output
+___________________
+How many people? 8
+How many pizzas do you have? 2
+
+8 people with 2 pizzas
+Each person gets 2 pieces of pizza.
+There are 0 leftover pieces.
+
+'''
+
+people = int(input('How many people? '))
+pizzas = int(input('How many pizzas do you have? '))
+slices = int(input('How many slices per pizza? '))
+
+pieces = (pizzas * slices) // people
+leftover = (pizzas * slices) % people
+
+print(f'''
+{people} people with {pizzas} pizzas.
+Each person gets {pieces} pieces of pizza.
+There are {leftover} leftover pieces.\
+''')
