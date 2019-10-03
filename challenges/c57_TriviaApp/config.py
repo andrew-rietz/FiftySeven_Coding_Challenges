28e183ea3f155928de02dd33a6cb20461a522916
Andrew Rietz <andrew.rietz@gmail.com>
2019-10-02 23:24:48 -0600

    Added REDIS variables

--- challenges/c57_TriviaApp/config.py
+++ challenges/c57_TriviaApp/config.py
@@ -9,6 +9,8 @@ class BaseConfig(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     REDIS_URL = os.environ.get('REDIS_URL')
+    ENDPOINT = os.environ.get("ENDPOINT")
+    POST_QUERY = os.environ.get("POST_QUERY")
 
 class DevelopmentConfig(BaseConfig):
     """Settings for our development config"""
