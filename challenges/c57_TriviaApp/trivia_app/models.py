0c3cd4e211fd104a09958809fa48edff343d1477
Andrew Rietz <andrew.rietz@gmail.com>
2019-10-02 23:25:48 -0600

    Model updates
    - Added comments about constructors
    - Renamed some import variables
    - Imported db

--- challenges/c57_TriviaApp/trivia_app/models.py
+++ challenges/c57_TriviaApp/trivia_app/models.py
@@ -1,17 +1,19 @@
 """Defines the database for the app"""
-
-from . import db
-from sqlalchemy.dialects.postgresql import JSON
-from flask import current_app, g
+from flask import current_app as app
+from flask import g as flask_g
 from flask.cli import with_appcontext
+from sqlalchemy.dialects.postgresql import JSON
+from . import db
 from datetime import datetime
 
 class User(db.Model):
+    # SQLAlchemy adds an implicit constructor to all model classes which accepts
+    # keyword arguments for all its columns and relationships
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), unique=True, nullable=False)
-    email = db.Column(sb.string(50), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.Text, nullable=False)
     type = db.Column(db.String(32), nullable=False)
 
@@ -19,6 +21,8 @@ class User(db.Model):
         return f"<User {self.username}>"
 
 class Question(db.Model):
+    # SQLAlchemy adds an implicit constructor to all model classes which accepts
+    # keyword arguments for all its columns and relationships
     __tablename__ = "questions"
 
     id = db.Column(db.Integer, primary_key=True)
