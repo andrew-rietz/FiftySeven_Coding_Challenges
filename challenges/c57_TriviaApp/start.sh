9c0e415ebd89948dbc79726cc3994b53876de0a9
Andrew Rietz <andrew.rietz@gmail.com>
2019-10-02 23:22:47 -0600

    Added DB migration commands

--- challenges/c57_TriviaApp/start.sh
+++ challenges/c57_TriviaApp/start.sh
@@ -2,4 +2,9 @@ source ./env_trivia/bin/activate
 source .env
 export FLASK_APP=trivia_app
 export FLASK_ENV=development
+# Only need to init the DB once -- then migrations
+# and upgrades should handle DB modifications
+# flask db init
+flask db migrate
+flask db upgrade
 flask run
