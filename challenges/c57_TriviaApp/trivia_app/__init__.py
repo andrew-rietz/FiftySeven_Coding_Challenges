8735e789f40ae607ecace3ee0a8c42191df961d7
Andrew Rietz <andrew.rietz@gmail.com>
2019-10-02 23:25:24 -0600

    Disabled REDIS, moved globals outside create_app

--- challenges/c57_TriviaApp/trivia_app/__init__.py
+++ challenges/c57_TriviaApp/trivia_app/__init__.py
@@ -1,33 +1,34 @@
 from flask import Flask
 from flask import g as flask_g
 from flask_sqlalchemy import SQLAlchemy
-from flask_session import Session
+# from flask_session import Session
 from flask_migrate import Migrate
 from flask_redis import FlaskRedis
 
+
+# Set globals
+db = SQLAlchemy()
+migrate = Migrate()
+# redis_store = FlaskRedis()
 
 def create_app():
     """Initialize the core application."""
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.DevelopmentConfig')
 
-    # Set globals
-    db = SQLAlchemy()
-    migrate = Migrate()
-    redis_store = FlaskRedis()
-
     with app.app_context():
         # Set global values
-        redis_store.endpoint = app.config['ENDPOINT']
-        redis_store.post_query = app.config['POST_QUERY']
+        # redis_store.endpoint = app.config['ENDPOINT']
+        # redis_store.post_query = app.config['POST_QUERY']
 
         # Initialize globals
-        redis_store.init_app(app)
+        # redis_store.init_app(app)
         db.init_app(app)
         migrate.init_app(app, db)
 
         # Register Blueprints
-        # app.register_blueprint(auth.auth_bp)
+        from . import users
+        app.register_blueprint(users.users_bp)
         # app.register_blueprint(admin.admin_bp)
 
-return app
+        return app
